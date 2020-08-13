ec7f2917d32f33379e6d129eaac0e5ed11b3d713
abdyrahmanow <begli.abd@gmail.com>
2020-08-13 22:10:52 +0500

    header responsive

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -53,38 +53,72 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="postcss" scoped>
 .header {
+  z-index: 90;
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  background-color: #fff;
   border-bottom: 1px solid rgb(var(--b6a));
 }
 .header-inner {
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
+  height: var(--header-height);
+  display: flex;
   align-items: center;
-  height: 60px;
+  justify-content: center;
+
+  @media (--t) {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    align-items: center;
+  }
 }
 .logo {
   font-size: 1.6rem;
   font-weight: bold;
 }
 .search {
-  text-align: center;
-}
-.search input {
-  width: 215px;
-  height: 28px;
-  border: 1px solid rgb(var(--b6a));
-  background-color: rgba(var(--b3f));
-  border-radius: 4px;
-  padding-left: 20px;
-  padding-right: 20px;
+  display: none;
+  @media (--t) {
+    display: block;
+    text-align: center;
+
+    input {
+      width: 215px;
+      height: 28px;
+      border: 1px solid rgb(var(--b6a));
+      background-color: rgba(var(--b3f));
+      border-radius: 4px;
+      padding-left: 20px;
+      padding-right: 20px;
+      padding-bottom: 2px;
+    }
+  }
 }
 .navigation {
+  z-index: 90;
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #fff;
+  border-top: 1px solid rgb(var(--b6a));
+  height: 70px;
   display: flex;
-  justify-content: flex-end;
   align-items: center;
-}
-.navigation a {
-  margin-left: 20px;
+  justify-content: space-around;
+
+  @media (--t) {
+    height: auto;
+    position: static;
+    justify-content: flex-end;
+    border: 0;
+
+    a {
+      margin-left: 20px;
+    }
+  }
 }
 </style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,11 +1,77 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
-    <CustomText>Salam</CustomText>
-    <CustomText tag="p">Salam</CustomText>
-    <CustomText tag="p" size="xsmall">Salam</CustomText>
-    <CustomText tag="p" size="xxsmall">Salam</CustomText>
-    <IconHome width="20" height="20" />
+    <div class="timeline">
+      <h1>Home</h1>
+      <CustomText>Salam</CustomText>
+      <CustomText tag="p">Salam</CustomText>
+      <CustomText tag="p" size="xsmall">Salam</CustomText>
+      <CustomText tag="p" size="xxsmall">Salam</CustomText>
+      <IconHome width="20" height="20" />
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. At magni, nulla
+        dignissimos aut suscipit minima ea deleniti praesentium qui optio vel
+        dolorum perspiciatis, fugit dolorem sint ex veniam expedita quos.
+        Aspernatur quam aperiam voluptatibus quidem. Temporibus impedit ducimus
+        tempore molestias, numquam fugit, nobis qui quis magnam, ipsam quos
+        quidem placeat accusamus corrupti laudantium laborum facilis praesentium
+        autem repudiandae fugiat perspiciatis? Quasi dolorem suscipit assumenda
+        quisquam, atque nihil alias vero esse, illum aut doloribus repellendus
+        totam nam sit maiores facere magni perspiciatis ea recusandae sunt
+        impedit eligendi cumque explicabo omnis. Voluptatibus.
+      </p>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. At magni, nulla
+        dignissimos aut suscipit minima ea deleniti praesentium qui optio vel
+        dolorum perspiciatis, fugit dolorem sint ex veniam expedita quos.
+        Aspernatur quam aperiam voluptatibus quidem. Temporibus impedit ducimus
+        tempore molestias, numquam fugit, nobis qui quis magnam, ipsam quos
+        quidem placeat accusamus corrupti laudantium laborum facilis praesentium
+        autem repudiandae fugiat perspiciatis? Quasi dolorem suscipit assumenda
+        quisquam, atque nihil alias vero esse, illum aut doloribus repellendus
+        totam nam sit maiores facere magni perspiciatis ea recusandae sunt
+        impedit eligendi cumque explicabo omnis. Voluptatibus.
+      </p>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. At magni, nulla
+        dignissimos aut suscipit minima ea deleniti praesentium qui optio vel
+        dolorum perspiciatis, fugit dolorem sint ex veniam expedita quos.
+        Aspernatur quam aperiam voluptatibus quidem. Temporibus impedit ducimus
+        tempore molestias, numquam fugit, nobis qui quis magnam, ipsam quos
+        quidem placeat accusamus corrupti laudantium laborum facilis praesentium
+        autem repudiandae fugiat perspiciatis? Quasi dolorem suscipit assumenda
+        quisquam, atque nihil alias vero esse, illum aut doloribus repellendus
+        totam nam sit maiores facere magni perspiciatis ea recusandae sunt
+        impedit eligendi cumque explicabo omnis. Voluptatibus.
+      </p>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. At magni, nulla
+        dignissimos aut suscipit minima ea deleniti praesentium qui optio vel
+        dolorum perspiciatis, fugit dolorem sint ex veniam expedita quos.
+        Aspernatur quam aperiam voluptatibus quidem. Temporibus impedit ducimus
+        tempore molestias, numquam fugit, nobis qui quis magnam, ipsam quos
+        quidem placeat accusamus corrupti laudantium laborum facilis praesentium
+        autem repudiandae fugiat perspiciatis? Quasi dolorem suscipit assumenda
+        quisquam, atque nihil alias vero esse, illum aut doloribus repellendus
+        totam nam sit maiores facere magni perspiciatis ea recusandae sunt
+        impedit eligendi cumque explicabo omnis. Voluptatibus.
+      </p>
+    </div>
+
+    <div class="sidebar">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+        possimus, porro aperiam illo mollitia, similique doloremque nesciunt
+        laborum hic ratione fugit. Laboriosam dolores dicta in consectetur ad
+        nemo magnam earum?Sapiente quas natus perspiciatis recusandae ullam
+        obcaecati autem suscipit laudantium? Voluptate, consequuntur harum
+        distinctio earum porro modi possimus recusandae in reiciendis, nulla
+        illo non officiis eius! Vitae nobis odit tempore!
+      </p>
+    </div>
   </div>
 </template>
 
@@ -21,11 +87,23 @@ export default {
 </script>
 
 <style scoped>
-h1 {
-  color: red;
+.home {
+  max-width: 605px;
+  margin-left: auto;
+  margin-right: auto;
 
   @media (--t) {
-    color: blue;
+    max-width: none;
+    display: grid;
+    grid-template-columns: 1fr 295px;
+    grid-gap: 30px;
+  }
+}
+.sidebar {
+  display: none;
+
+  @media (--t) {
+    display: block;
   }
 }
 </style>
